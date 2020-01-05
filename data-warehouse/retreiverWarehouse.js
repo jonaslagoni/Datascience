88ad365f9a88b572314ef5fc14f856c8109aab7e
mathn16 <Mathias626@hotmail.com>
2020-01-05 22:47:11 +0100

    updates

--- data-warehouse/retreiverWarehouse.js
+++ data-warehouse/retreiverWarehouse.js
@@ -1,26 +1,24 @@
 const axios = require('axios');
+const kafka = require('./kafka/index');
+
+const energiProductionSchema = require("./kafka/schemas/EnerginetProductionAndExchangeSchema");
+const energiSpotPricesSchema = require("./kafka/schemas/EnerginetElspotSchema");
+const energiEmissionSchema = require("./kafka/schemas/EnerginetCO2EmissionSchema");
 
 let url = 'https://api.energidataservice.dk/datastore_search'
 let elementsStored = 0;
 
-
-var data = {
-    resource_id: 'electricityprodex5minrealtime',
-    limit: 5
-};
-
-ajax({
-    url,
-    data: data,
-    dataType: 'jsonp',
-    
-}, function(err, res, body) {
-    if(err){
-
-    }else if(res){
-        
-    }else if(body){
-
+axios.get('https://api.energidataservice.dk/datastore_search', {
+    params: {
+        resource_id: 'electricityprodex5minrealtime',
+        limit: 5
     }
 })
-
+.then(function(response) {
+    console.log(response.data.result.records);
+    response.data.result.records.forEach(element => {
+        const energiSchema = new energiProductionSchema(element.Minutes5DK, element.PriceAream, element.ProductionLt100MW, element.ProductionGe100MW)
+        kafka.publishEnergidataProductionAndExchange(energiSchema);
+        console.log(energiSchema);
+    });
+});
