588d136c15c60527d8c2d2384593c9eaf8eebf40
mathn16 <Mathias626@hotmail.com>
2020-01-06 00:28:53 +0100

    Update retreiverWarehouse.js

--- data-warehouse/retreiverWarehouse.js
+++ data-warehouse/retreiverWarehouse.js
@@ -14,13 +14,62 @@ axios.get('https://api.energidataservice.dk/datastore_search', {
         limit: 5
     }
 })
-.then(function(response) {
+.then((response) => {
     console.log(response.data.result.records);
     response.data.result.records.forEach(element => {
         const energiSchema = new energiProductionSchema();
-        energiSchema.setData(element.Minutes5DK, element.PriceAream, element.ProductionLt100MW, element.ProductionGe100MW)
-        kafka.publishEnergidataProductionAndExchange(energiSchema);
-        console.log(energiSchema);
+        energiSchema.setData(element.Minutes5DK, element.PriceArea, element.ProductionLt100MW, element.ProductionGe100MW)
+        kafka.publishEnergidataProductionAndExchange(JSON.stringify(energiSchema)).then(() => {
+            console.log("Done: " + energiSchema);
+        }).catch(e => {
+            console.log(e);
+        });
     });
 })
-.catch();
+.catch((e) =>{
+    console.log(e);
+});
+
+axios.get('https://api.energidataservice.dk/datastore_search', {
+    params: {
+        resource_id: 'elspotprices',
+        limit: 5
+    }
+})
+.then((response) => {
+    console.log(response.data.result.records);
+    response.data.result.records.forEach(element => {
+        const spotPriceSchema = new energiSpotPricesSchema();
+        spotPriceSchema.setData(element.HourDK, element.PriceArea, element.SpotPriceEUR)
+        kafka.publishEnergidataProductionAndExchange(JSON.stringify(spotPriceSchema)).then(() => {
+            console.log("Done: " + spotPriceSchema);
+        }).catch(e => {
+            console.log(e);
+        });
+    });
+})
+.catch((e) =>{
+    console.log(e);
+});
+
+axios.get('https://api.energidataservice.dk/datastore_search', {
+    params: {
+        resource_id: 'co2emis',
+        limit: 5
+    }
+})
+.then((response) => {
+    console.log(response.data.result.records);
+    response.data.result.records.forEach(element => {
+        const emisSchema = new energiEmissionSchema();
+        emisSchema.setData(element.Minutes5DK, element.PriceArea, element.CO2Emission)
+        kafka.publishEnergidataProductionAndExchange(JSON.stringify(emisSchema)).then(() => {
+            console.log("Done: " + emisSchema);
+        }).catch(e => {
+            console.log(e);
+        });
+    });
+})
+.catch((e) =>{
+    console.log(e);
+});
