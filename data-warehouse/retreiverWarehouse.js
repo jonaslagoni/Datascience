d1488336b84176a9f304f0ecd76701ea372d6e15
jonaslagoni <jonas-lt@live.dk>
2020-01-10 19:42:36 +0100

    Fixed wrong data being send.

--- data-warehouse/retreiverWarehouse.js
+++ data-warehouse/retreiverWarehouse.js
@@ -1,9 +1,12 @@
 const axios = require('axios');
 const kafka = require('./kafka/index');
 
-const energiProductionSchema = require("./kafka/schemas/EnerginetProductionAndExchangeSchema");
-const energiSpotPricesSchema = require("./kafka/schemas/EnerginetElspotSchema");
-const energiEmissionSchema = require("./kafka/schemas/EnerginetCO2EmissionSchema");
+const energiProductionSchema = require('./kafka/schemas/EnerginetProductionAndExchangeSchema');
+const energiSpotPricesSchema = require('./kafka/schemas/EnerginetElspotSchema');
+const energiEmissionSchema = require('./kafka/schemas/EnerginetCO2EmissionSchema');
+const energiProduction = require('./kafka/messages/EnerginetProductionAndExchange');
+const energiSpotPrices = require('./kafka/messages/EnerginetElspot');
+const energiEmission = require('./kafka/messages/EnerginetCO2Emission');
 
 let productionElementsStored = 0;
 let emissionElementsStored = 0;
@@ -12,103 +15,139 @@ let electricityprodex5minrealtimeTimeout = 5000;
 let elspotpricesTimeout = 5000;
 let co2emisTimeout = 5000;
 function waitForKafka() {
-    if (kafka.isConnected) {
-        setInterval(function () {
-            axios.get('https://api.energidataservice.dk/datastore_search', {
-                params: {
-                    resource_id: 'electricityprodex5minrealtime',
-                    limit: 5,
-                    offset: productionElementsStored
-                }
-            })
-            .then((response) => {
-                let counter;
-                console.log(response.data.result.records);
-                if (productionElementsStored != 0) {
-                    counter = response.data.result.records.length - productionElementsStored;
-                } else {
-                    counter = productionElementsStored;
-                }
-                response.data.result.records.forEach(element => {
-                    kafka.publishEnergidataProductionAndExchange(JSON.stringify(element)).then(() => {
-                        console.log("Done: " + energiSchema);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-                });
-                productionElementsStored = response.data.result.records.length;
-            })
-            .catch((e) => {
-                console.log(e);
-            })
-        }, electricityprodex5minrealtimeTimeout);
+	if (kafka.isConnected) {
+		setInterval(function() {
+			axios
+				.get('https://api.energidataservice.dk/datastore_search', {
+					params: {
+						resource_id: 'electricityprodex5minrealtime',
+						limit: 5,
+						offset: productionElementsStored
+					}
+				})
+				.then(response => {
+					let counter;
+					console.log(response.data.result.records);
+					if (productionElementsStored != 0) {
+						counter =
+							response.data.result.records.length - productionElementsStored;
+					} else {
+						counter = productionElementsStored;
+					}
+					response.data.result.records.forEach(element => {
+						const payload = new energiProduction();
+						const energiSchema = new energiProductionSchema();
+						energiSchema.setData(
+							element.Minutes5DK,
+							element.PriceArea,
+							element.ProductionLt100MW,
+							element.ProductionGe100MW
+						);
+						payload.setData(energiSchema);
+						kafka
+							.publishEnergidataProductionAndExchange(JSON.stringify(payload))
+							.then(() => {
+								console.log('Done: ' + energiSchema);
+							})
+							.catch(e => {
+								console.log(e);
+							});
+					});
+					productionElementsStored = response.data.result.records.length;
+				})
+				.catch(e => {
+					console.log(e);
+				});
+		}, electricityprodex5minrealtimeTimeout);
 
-        setInterval(function () {
-            axios.get('https://api.energidataservice.dk/datastore_search', {
-                params: {
-                    resource_id: 'elspotprices',
-                    limit: 5,
-                    offset: elspotElementsStored
-                }
-            })
-            .then((response) => {
-                let counter;
-                if (elspotElementsStored != 0) {
-                    counter = response.data.result.records.length - elspotElementsStored;
-                } else {
-                    counter = elspotElementsStored;
-                }
-                console.log(response.data.result.records);
-                response.data.result.records.forEach(element => {
-                    const spotPriceSchema = new energiSpotPricesSchema();
-                    spotPriceSchema.setData(element.HourDK, element.PriceArea, element.SpotPriceEUR)
-                    kafka.publishEnergidataElspot(JSON.stringify(spotPriceSchema)).then(() => {
-                        console.log("Done: " + spotPriceSchema);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-                });
-                elspotElementsStored = response.data.result.records.length;
-            })
-            .catch((e) => {
-                console.log(e);
-            })
-        }, elspotpricesTimeout);
-        setInterval(function () {
-            axios.get('https://api.energidataservice.dk/datastore_search', {
-                params: {
-                    resource_id: 'co2emis',
-                    limit: 5,
-                    offset: emissionElementsStored
-                }
-            })
-            .then((response) => {
-                let counter;
-                if (emissionElementsStored != 0) {
-                    counter = response.data.result.records.length - emissionElementsStored;
-                } else {
-                    counter = emissionElementsStored;
-                }
-                console.log(response.data.result.records);
-                response.data.result.records.forEach(element => {
-                    const emisSchema = new energiEmissionSchema();
-                    emisSchema.setData(element.Minutes5DK, element.PriceArea, element.CO2Emission)
-                    kafka.publishEnergidataCo2Emission(JSON.stringify(emisSchema)).then(() => {
-                        console.log("Done: " + emisSchema);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-                });
-                emissionElementsStored = response.data.result.records.length;
-            })
-            .catch((e) => {
-                console.log(e);
-            })
-        }, co2emisTimeout);
+		setInterval(function() {
+			axios
+				.get('https://api.energidataservice.dk/datastore_search', {
+					params: {
+						resource_id: 'elspotprices',
+						limit: 5,
+						offset: elspotElementsStored
+					}
+				})
+				.then(response => {
+					let counter;
+					if (elspotElementsStored != 0) {
+						counter =
+							response.data.result.records.length - elspotElementsStored;
+					} else {
+						counter = elspotElementsStored;
+					}
+					console.log(response.data.result.records);
+					response.data.result.records.forEach(element => {
+						const payload = new energiSpotPrices();
+						const spotPriceSchema = new energiSpotPricesSchema();
+						spotPriceSchema.setData(
+							element.HourDK,
+							element.PriceArea,
+							element.SpotPriceEUR
+						);
+						payload.setData(spotPriceSchema);
+						kafka
+							.publishEnergidataElspot(JSON.stringify(payload))
+							.then(() => {
+								console.log('Done: ' + spotPriceSchema);
+							})
+							.catch(e => {
+								console.log(e);
+							});
+					});
+					elspotElementsStored = response.data.result.records.length;
+				})
+				.catch(e => {
+					console.log(e);
+				});
+		}, elspotpricesTimeout);
+		setInterval(function() {
+			axios
+				.get('https://api.energidataservice.dk/datastore_search', {
+					params: {
+						resource_id: 'co2emis',
+						limit: 5,
+						offset: emissionElementsStored
+					}
+				})
+				.then(response => {
+					let counter;
+					if (emissionElementsStored != 0) {
+						counter =
+							response.data.result.records.length - emissionElementsStored;
+					} else {
+						counter = emissionElementsStored;
+					}
+					console.log(response.data.result.records);
+					response.data.result.records.forEach(element => {
+						const payload = new energiEmission();
+						const emisSchema = new energiEmissionSchema();
+						emisSchema.setData(
+							element.Minutes5DK,
+							element.PriceArea,
+							element.CO2Emission
+						);
+						payload.setData(emisSchema);
 
-    } else {
-        setTimeout(waitForKafka, 5000);
-    }
+						kafka
+							.publishEnergidataCo2Emission(JSON.stringify(payload))
+							.then(() => {
+								console.log('Done: ' + emisSchema);
+							})
+							.catch(e => {
+								console.log(e);
+							});
+					});
+					emissionElementsStored = response.data.result.records.length;
+				})
+				.catch(e => {
+					console.log(e);
+				});
+		}, co2emisTimeout);
+	} else {
+		setTimeout(waitForKafka, 5000);
+	}
 }
 
 waitForKafka();
